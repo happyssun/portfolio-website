291256052fa41110e20ce1596c22dfb6eee6682e
Sun Choi <sunyfun1125@gmail.com>
2023-09-03 19:27:29 +0200

    project part

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import About from "@/components/about";
-import Experience from "@/components/experience";
+
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 
 import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -15,9 +14,6 @@ export default function Home() {
       <SectionDivider />
       <Projects />
       <SectionDivider />
-      <Skills />
-      <SectionDivider />
-      <Experience />
     </main>
   );
 }
